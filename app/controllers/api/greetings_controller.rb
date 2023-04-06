bb9f72e64c262ec11457785603096be61091eb2c
Soe Sandar Win <soesandarwin2201@gmail.com>
2023-04-06 11:12:30 +0630

    fix the linters errors

--- app/controllers/api/greetings_controller.rb
+++ app/controllers/api/greetings_controller.rb
@@ -1,6 +1,6 @@
 class Api::GreetingsController < Api::ApplicationController
-     def index
-       @greetings = Greeting.find_by(id: rand(1..Greeting.count))
-       render json: @greetings
-     end
+  def index
+    @greetings = Greeting.find_by(id: rand(1..Greeting.count))
+    render json: @greetings
+  end
 end
